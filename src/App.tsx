3c6a84423b05e251b67e8e9b118d0b9faec73348
e <2182620965@qq.com>
2020-02-24 11:57:45 +0800

    拷贝dataink_test的内容

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout } from 'antd';
+import Sidebar from './UI/Sidebar'
+import SplitPane from 'react-split-pane';
+import { PaperContainer, Circle, Layer } from '@psychobolt/react-paperjs';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const { Header, Sider, Content } = Layout;
+const Shapes = () => <Circle center={[120, 50]} radius={35} fillColor="#00FF00" />;
+
+const App = () => {
+    return (
+        <Layout className="me-layout">
+            <Header className="me-header">
+                ChartInk 0.1
+            </Header>
+            <Layout>
+                <Sider width={48} className="me-left-bar">
+                    <Sidebar />
+                </Sider>
+                <Content className="me-canvas">
+                    <PaperContainer>
+                        <Circle center={[80, 50]} radius={35} fillColor="red" />
+                        <Layer>
+                            <Shapes />
+                        </Layer>
+                    </PaperContainer>
+                </Content>
+                <Content className="me-right-bar">
+                    <SplitPane
+                        defaultSize="60%"
+                        split="horizontal"
+                        style={{ position: 'static' }}
+                        resizerStyle={{ padding: '5px' }}
+                        paneStyle={{ background: '#eee' }}
+                        pane2Style={{ background: '#aaa4ba' }}
+                    >
+                        <div />
+                        <div />
+                    </SplitPane>
+                </Content>
+            </Layout>
+        </Layout>
+    );
 }
 
 export default App;
